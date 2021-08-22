ca1a7139b19f6d7a71139b73966656befd7ca1e9
Liberbell <liberbell@gmail.com>
2021-08-22 15:09:56 +0900

    views7

--- first/myapp/views.py
+++ first/myapp/views.py
@@ -10,7 +10,7 @@ def index(request):
     return render(request, "myapp/index.html", context)
 
 def about(request):
-    return render(request, "myapp/index.html")
+    return render(request, "myapp/about.html")
 
 def info(request):
     return render(request, "myapp/index.html")
