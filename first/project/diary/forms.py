5b4745de7aff355716ece2d8cf213c9b9a203f46
Liberbell <liberbell@gmail.com>
2021-08-24 08:17:27 +0900

    forms2

--- first/project/diary/forms.py
+++ first/project/diary/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.db import models
-from django.db.models import fields
+from django import forms
 from .models import Day
 
 class DayCreateForm(models.ModelForm):
