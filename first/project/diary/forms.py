1edf83f6b1c9d29442541350bc6cfe854e21d470
Liberbell <liberbell@gmail.com>
2021-08-24 08:09:05 +0900

    forms1

---
+++ first/project/diary/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from django.db import models
+from django.db.models import fields
+from .models import Day
+
+class DayCreateForm(models.ModelForm):
+    class Meta:
+        model = Day
+        fields = "__all__"
