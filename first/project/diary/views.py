4329df098e558dd4b7edc76649a77567de4a8c09
Liberbell <liberbell@gmail.com>
2021-08-25 08:15:31 +0900

    views1

--- first/project/diary/views.py
+++ first/project/diary/views.py
@@ -1,11 +1,15 @@
 from .forms import DayCreateForm
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def index(request):
     return render(request, "diary/day_list.html")
 
 def add(request):
+    form = DayCreateForm(request.POST or None)
+    if request.method == "POST" and form.is_valid:
+        form.save()
+        
     context = {
         'form': DayCreateForm()
     }
