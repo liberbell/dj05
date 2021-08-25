6ee577a6034ef95106727048a3184998206f386a
Liberbell <liberbell@gmail.com>
2021-08-26 08:48:24 +0900

    views1

--- first/project/diary/views.py
+++ first/project/diary/views.py
@@ -1,5 +1,5 @@
 from .forms import DayCreateForm
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Day
 
 # Create your views here.
