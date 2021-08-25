5db14a539faa73d1366f13577c2ad959f5f42774
Liberbell <liberbell@gmail.com>
2021-08-26 08:49:18 +0900

    views2

--- first/project/diary/views.py
+++ first/project/diary/views.py
@@ -19,3 +19,6 @@ def add(request):
         'form': DayCreateForm()
     }
     return render(request, "diary/day_form.html", context)
+
+def update(request, pk):
+    day = get_object_or_404(Day, pk=pk)
