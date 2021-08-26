3c5cdf5965b23a26906ca851a0a35ebe53743d99
Liberbell <liberbell@gmail.com>
2021-08-27 08:42:00 +0900

    views2

--- first/project/diary/views.py
+++ first/project/diary/views.py
@@ -23,7 +23,7 @@ def add(request):
 def update(request, pk):
     day = get_object_or_404(Day, pk=pk)
 
-    form = DayCreateForm(request.Post or None, instance=day)
+    form = DayCreateForm(request.POST or None, instance=day)
 
     if request.method == "POST" and form.is_valid:
         form.save()
