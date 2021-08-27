6942b611e9adfffc8297d65b4d54934845f07e71
Liberbell <liberbell@gmail.com>
2021-08-27 09:06:57 +0900

    views2

--- first/project/diary/views.py
+++ first/project/diary/views.py
@@ -45,3 +45,15 @@ def delete(request, pk):
         "day": day,
     }
     return render(request, "diary/day_confirm_delete.html", context)
+
+def detail(request, pk):
+    day = get_object_or_404(Day, pk=pk)
+
+    if request.method == "POST":
+        day.delete()
+        return redirect("diary:index")
+
+    context = {
+        "day": day,
+    }
+    return render(request, "diary/day_detail.html", context)
