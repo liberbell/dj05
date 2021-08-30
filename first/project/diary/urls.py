75d6fb8de086b07501b3a5a5e9ba97bc629b8d93
Liberbell <liberbell@gmail.com>
2021-08-31 07:57:48 +0900

    urls1

--- first/project/diary/urls.py
+++ first/project/diary/urls.py
@@ -6,7 +6,7 @@ app_name = "diary"
 urlpatterns = [
     path("", views.indexView.as_view(), name="index"),
     # path("add/", views.AddView.as_view(), name="add"),
-    # path("update/<int:pk>/", views.UpdateView.as_view(), name="update"),
-    # path("delete/<int:pk>/", views.DeleteView.as_view(), name="delete"),
+    path("update/<int:pk>/", views.UpdateView.as_view(), name="update"),
+    path("delete/<int:pk>/", views.DeleteView.as_view(), name="delete"),
     path("detail/<int:pk>", views.DetailView.as_view(), name="detail"),
 ]
