f74c08f527d8f19c0e6fe6f3be482fcd3edc8722
Liberbell <liberbell@gmail.com>
2021-08-30 09:19:08 +0900

    model1

--- first/project/diary/models.py
+++ first/project/diary/models.py
@@ -6,3 +6,6 @@ class Day(models.Model):
     title = models.CharField("title", max_length=200)
     text = models.TextField("body")
     date = models.DateTimeField("date", default=timezone.now)
+
+    def __str__(self):
+        return self.title
