183456163633dd4e85f40bb9ae07baa4c7f63d9f
Liberbell <liberbell@gmail.com>
2021-09-01 08:56:55 +0900

    models1

--- first/project2/employee/models.py
+++ first/project2/employee/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.db.models.fields import DateTimeField
+from django.utils import timezone
 
 # Create your models here.
+class Department(models.Model):
+    name = models.CharField("Dep Name", max_length=20)
+    created_at = models.DateTimeField("Date", default=timezone.now)
