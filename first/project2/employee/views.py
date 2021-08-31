6bfc80072f29a2ac454e3b4d3c6f269a93e7c4af
Liberbell <liberbell@gmail.com>
2021-09-01 08:40:59 +0900

    views2

--- first/project2/employee/views.py
+++ first/project2/employee/views.py
@@ -1,5 +1,7 @@
+from django import template
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
-def IndexView(request):
-    return render(request, "index.html")
+class IndexView(generic.TemplateView):
+    template_name = "employee/employee_list.html"
