835d56709bb82c1c27ea261b8bb53891acd49fde
Liberbell <liberbell@gmail.com>
2021-09-01 08:59:13 +0900

    models3

--- first/project2/employee/models.py
+++ first/project2/employee/models.py
@@ -9,4 +9,9 @@ class Department(models.Model):
 
     def __str__(self):
         return self.name
+
+class Employee(models.Model):
+    first_name = models.CharField("First name", max_length=20)
+    last_name = models.CharField("Last name", max_length=20)
+    email = models.EmailField("E-mail", max_length=254, blank=True)
     
