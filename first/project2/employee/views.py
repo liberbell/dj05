6b7e9aed18e21cf4cc0984589dc6f67524c757ca
Liberbell <liberbell@gmail.com>
2021-09-02 08:49:46 +0900

    views4

--- first/project2/employee/views.py
+++ first/project2/employee/views.py
@@ -1,6 +1,3 @@
-from first.project2.employee.models import Employee
-from django import template
-from django.shortcuts import render
 from django.views import generic
 from .models import Employee
 
