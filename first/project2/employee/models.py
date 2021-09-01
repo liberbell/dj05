8b12f3b5b037143c7beda0ace0fd3a6fe1068089
Liberbell <liberbell@gmail.com>
2021-09-02 08:54:40 +0900

    models3

--- first/project2/employee/models.py
+++ first/project2/employee/models.py
@@ -10,6 +10,13 @@ class Department(models.Model):
     def __str__(self):
         return self.name
 
+class Club(models.Model):
+    name = models.CharField("Club Name", max_length=20)
+    created_at = models.DateTimeField("Date", default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
 class Employee(models.Model):
     first_name = models.CharField("First name", max_length=20)
     last_name = models.CharField("Last name", max_length=20)
@@ -17,6 +24,7 @@ class Employee(models.Model):
     department = models.ForeignKey(
         Department, verbose_name="Dep Name", on_delete=models.PROTECT
     )
+    club = models.ManyToManyField(Club, verbose_name="Club Name")
     created_at = models.DateTimeField("Date", default=timezone.now)
 
     def __str__(self):
