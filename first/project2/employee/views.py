6466f34133f187363947191f752cfba649f14783
Liberbell <liberbell@gmail.com>
2021-09-02 09:15:13 +0900

    views4

--- first/project2/employee/views.py
+++ first/project2/employee/views.py
@@ -1,6 +1,12 @@
+from first.project2.employee.forms import SearchForm
 from django.views import generic
 from .models import Employee
 
 # Create your views here.
 class IndexView(generic.ListView):
     model = Employee
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        context["form"] = SearchForm(self.request.GET)
+        return context
