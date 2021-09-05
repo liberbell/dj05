77a0d02d2276af39172ec98086207c942bfa4d29
Liberbell <liberbell@gmail.com>
2021-09-05 10:59:16 +0900

    views2

--- first/project3/blog/views.py
+++ first/project3/blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
 from django.views import generic
+from .models import Post
 
 # Create your views here.
 class IndexView(generic.ListView):
-    template_name = "blog/post_list.html"
+    model = Post
+    # template_name = "blog/post_list.html"
