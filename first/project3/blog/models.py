bc769cacffefe84a84d021eb17c46d4330b7c22a
Liberbell <liberbell@gmail.com>
2021-09-05 10:53:55 +0900

    models3

--- first/project3/blog/models.py
+++ first/project3/blog/models.py
@@ -2,7 +2,20 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField("Category name"), max_length=255)
+    created_at = models.DateTimeField("Created date", default=timezone.now)
+
+    def __str__(self):
+        return self.name
+    
+
 class Post(models.Model):
     title = models.CharField("Title", max_length=255)
     text = models.TextField("Body")
     created_at = models.DateTimeField("Created date", default=timezone.now)
+
+    def __str__(self):
+        return self.title
+    
