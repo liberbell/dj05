03cf63119b94960130564aad1f5dbf611f3c8399
Liberbell <liberbell@gmail.com>
2021-09-06 08:59:24 +0900

    views2

--- first/project3/blog/views.py
+++ first/project3/blog/views.py
@@ -6,6 +6,6 @@ class IndexView(generic.ListView):
     model = Post
     # template_name = "blog/post_list.html"
 
-    def def get_queryset(self):
+    def get_queryset(self):
         return Post.objects.order_by("-created at")
     
