03891c7f28adce2e910ce41f22b342591c812713
Liberbell <liberbell@gmail.com>
2021-09-09 08:46:22 +0900

    forms2

--- first/project3/blog/forms.py
+++ first/project3/blog/forms.py
@@ -1,9 +1,9 @@
 from django import forms
 from django.db import models
 from django.db.models import fields
-from models import Comment
+from .models import Comment
 
-class CommentCreateForms(models.ModelForm):
+class CommentCreateForms(forms.ModelForm):
 
     class Meta:
         model = Comment
