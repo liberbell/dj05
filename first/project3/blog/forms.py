b3f9c07a0f94d366024b7a11562b42371a258028
Liberbell <liberbell@gmail.com>
2021-09-10 08:38:47 +0900

    forms1

--- first/project3/blog/forms.py
+++ first/project3/blog/forms.py
@@ -5,6 +5,11 @@ from .models import Comment
 
 class CommentCreateForms(forms.ModelForm):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control"
+
     class Meta:
         model = Comment
         fields = ["name", "text"]
