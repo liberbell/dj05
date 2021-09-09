b95519a824650e2ad0d54e925dd31e884c04c43e
Liberbell <liberbell@gmail.com>
2021-09-09 09:13:47 +0900

    views6

--- first/project3/blog/views.py
+++ first/project3/blog/views.py
@@ -1,5 +1,5 @@
 from django.views import generic
-from django.db.models import Q
+from django.db.models import Q, fields
 from django.shortcuts import get_object_or_404, redirect
 from .models import Post, Category, Comment
 from .forms import CommentCreateForms
@@ -34,12 +34,17 @@ class DetailView(generic.DetailView):
     model = Post
 
 class CommentView(generic.CreateView):
-    model = Comment
-    form_class = CommentCreateForms
+    # model = Comment
+    # form_class = CommentCreateForms
 
-    def form_valid(self, form):
-        post_pk = self.kwargs["post_pk"]
-        comment = form.save(commit=False)
-        comment.post = get_object_or_404(Post, pk=post_pk)
-        comment.save()
-        return redirect("blog:detail", pk=post_pk)
+    # def form_valid(self, form):
+    #     post_pk = self.kwargs["post_pk"]
+    #     comment = form.save(commit=False)
+    #     comment.post = get_object_or_404(Post, pk=post_pk)
+    #     comment.save()
+    #     return redirect("blog:detail", pk=post_pk)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            fields.widget.attrs["class"] = "form-control"
