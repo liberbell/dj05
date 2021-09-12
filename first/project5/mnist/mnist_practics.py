50d5a6fa6a89c1739a57b158550b33a2bdf39f8c
Liberbell <liberbell@gmail.com>
2021-09-12 13:34:17 +0900

    mnistpractice1

--- first/project5/mnist/mnist_practics.py
+++ first/project5/mnist/mnist_practics.py
@@ -12,11 +12,14 @@ def read():
 def create_and_save():
     mnist = datasets.fetch_mldata("MNIST original", data_home="image/")
     X = mnist.data / 255
-    Y = mnist.target
+    y = mnist.target
 
     X_train, X_test, y_train, y_test = train_test_split(
-    X, y, train_size=1000, test_size=300
+    X, y, train_size=50000, test_size=300
     )
+
+    clf = svm.SVC()
+    clf.fit(X_train, y_train)
 
 # サンプル画像データのロード
 mnist = datasets.fetch_openml('mnist_784', data_home='image/')
