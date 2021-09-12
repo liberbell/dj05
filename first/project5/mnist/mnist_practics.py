9549e291ea24ac0ee4339bff6b06455f3f5a3d4c
Liberbell <liberbell@gmail.com>
2021-09-12 13:35:31 +0900

    ministpractice2

--- first/project5/mnist/mnist_practics.py
+++ first/project5/mnist/mnist_practics.py
@@ -21,20 +21,24 @@ def create_and_save():
     clf = svm.SVC()
     clf.fit(X_train, y_train)
 
-# サンプル画像データのロード
-mnist = datasets.fetch_openml('mnist_784', data_home='image/')
-X = mnist.data / 255
-y = mnist.target
+    with("mnist", "wb") as file:
+        pickle.dump(clf, file)
+    return clf
 
-# 訓練用データとテスト用データに分ける
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y, train_size=1000, test_size=300
-)
+# # サンプル画像データのロード
+# mnist = datasets.fetch_openml('mnist_784', data_home='image/')
+# X = mnist.data / 255
+# y = mnist.target
 
-# 訓練用データで学習
-clf = svm.SVC()
-clf.fit(X_train, y_train)
+# # 訓練用データとテスト用データに分ける
+# X_train, X_test, y_train, y_test = train_test_split(
+#     X, y, train_size=1000, test_size=300
+# )
 
-# テスト用データで実際に試す
-score = clf.score(X_test, y_test)
-print(score)
+# # 訓練用データで学習
+# clf = svm.SVC()
+# clf.fit(X_train, y_train)
+
+# # テスト用データで実際に試す
+# score = clf.score(X_test, y_test)
+# print(score)
